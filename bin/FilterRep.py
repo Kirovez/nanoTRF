d0ad8902283f31cf7d51bf2df9bc44a67d1385d9
Elizaveta Kolganova <57593031+liza-alpinia@users.noreply.github.com>
2020-07-23 16:40:36 +0300

    Filtration on the cluster abundancy

--- bin/FilterRep.py
+++ bin/FilterRep.py
@@ -17,6 +17,7 @@ class FilteringLouvTab():
         
     def createListRep(self):
         listFiltRep={} 
+        dictRep={}
         cluster_abundancy = defaultdict(int)
         with open(self.clustering_outTab) as LouvTab:
             for seqId in LouvTab: ### seq_id*rep0*len*nrepeats \t cluster 
@@ -29,16 +30,14 @@ class FilteringLouvTab():
                     if countRep>5:
                 #d4e9d1ee-f7b1-48e7-b8b8-dc7f10aa8435*rep0*368*2.4/0
                         listFiltRep[seq_id] = numCl
-        listFiltRep = {i:listFiltRep[i] for i in listFiltRep if cluster_abundancy[listFiltRep[i]] > self.minAbundancy}
-        return listFiltRep
+        dictRep = {'*'.join(i.split('*')[0:2]):listFiltRep[i] for i in listFiltRep if cluster_abundancy[listFiltRep[i]] > float(self.minAbundancy)}
+        return dictRep
    
-    
-    def main(self,list_Rep):
+        def main(self,list_Rep):
         with open(self.filtering_outTab,'w') as fastaWr:
             seq_monomer = {}
             for seq in SeqIO.parse(self.THall_monomers, 'fasta'):
-                seqID = '*'.join(seq.id.split('_')[0:2])
-             
+                seqID = '*'.join(seq.id.split('_')[0:2])             
                 if seqID not in seq_monomer:
                     seq_monomer[seqID] = defaultdict(str)
                 seq_monomer[seqID][seq.description] = str(seq.seq)           
@@ -46,13 +45,6 @@ class FilteringLouvTab():
                 nClust=list_Rep[reSeq]
                 if reSeq in seq_monomer:
                     for key in seq_monomer[reSeq]:                   
-                        reFSeqMnm = '>{0}/{1}\n{2}\n'.format(key, nClust, seq_monomer[reSeq][key]')
-              
+                        reFSeqMnm = '>{0}/{1}\n{2}\n'.format(key, nClust, seq_monomer[reSeq][key])              
                         fastaWr.write(reFSeqMnm)
-      
-                        reFSeqMnm = '>{0}/{1}{2}{3}'.format(key, nClust, seq_monomer[reSeq][key],'\n')
-                        fastaWr.write(reFSeqMnm)
-                        
-    
-    
-
+               
