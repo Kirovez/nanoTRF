f2f198895129373b0001b0ba67deb5a9b0701e74
Elizaveta Kolganova <57593031+liza-alpinia@users.noreply.github.com>
2020-07-30 11:35:36 +0300

    Update nanoTRF.py

--- nanoTRF.py
+++ nanoTRF.py
@@ -6,13 +6,14 @@ from bin import Consensus_Assembly
 from bin.helpers.help_functions import checkDir_or_create
 from bin.helpers.help_functions import getLog
 from bin import run_BLAST
+from bin import Delete_dir
+import os
 
 class nanoTRF():
-    def __init__(self, reads,  out_directory="test",
-                 threads = 4, pathTH = r'/home/ikirov/Tools/TideHunter-v1.4.2/bin/TideHunter'):
+    def __init__(self, reads,  out_directory,minAbundancy,consensus_name,threads, pathTH ,log_name,min_overlap, opt_delete):
         self.outDirectory = checkDir_or_create(out_directory)
         self.reads = reads
-        self.log_file = self.outDirectory + '/loging.log'
+        self.log_file = self.outDirectory + '/' + log_name
         LOG = getLog(self.log_file, 'nanoTRF')
         LOG.info("nanoTRF started...")
         self.read_data = ''
@@ -39,7 +40,14 @@ class nanoTRF():
         ##########################################
         ###############CLUSTERING################
         self.clustering_outTab = ''
-
+        #########################################
+        ##############FILTERING##################
+        self.minAbundancy=minAbundancy
+        ###############CANU#####################
+        self.min_overlap = min_overlap
+        self.consensus_name = self.outDirectory + '/'+consensus_name
+        self.opt_delete = opt_delete
+        
         ###MAIN###
         self.main()
 
@@ -61,32 +69,51 @@ class nanoTRF():
         self.edge_list_after_blast_file = blast_module_data.edge_list_file
 
 
-        ##Clusetering##
+        ##Clustering##
         louv_module_data=Louv_clustering.LouvClustering(self.edge_list_after_blast_file, self.outDirectory, self.log_file)
 
         ###Filtering##
         self.clustering_outTab=louv_module_data.clustering_outTab
-        self.Filt_data=FilterRep.FilteringLouvTab(self.clustering_outTab,self.outDirectory,self.TH_all_monomers)
+        Filt_data=FilterRep.FilteringLouvTab(self.clustering_outTab,self.outDirectory,self.TH_all_monomers,self.minAbundancy,self.log_file)
         self.tableFilt=Filt_data.filtering_outTab
 
 
         ###Canu###
-        Consensus_Assembly.ConsAssembly(self.tableFilt,self.outDirectory)
+        Consensus_Assembly.ConsAssembly(self.tableFilt,self.outDirectory,self.log_file,self.min_overlap, self.consensus_name)
+        self.dir_clust=consensus_out.outdir_clust
+        self.dir_canu=consensus_out.outdir_canu
+        ###Delete directories###
+       
+        Delete_dir.Delete_direct(self.dir_clust,self.dir_canu,self.opt_delete,self.log_file)
 
 
 
 
 if __name__ == "__main__":
     import argparse
-    import os.path
+    import os
+
     parser = argparse.ArgumentParser(description='A tool to clustering sequences in fasta file and searching  '
                                                  'consensus among the many sequences for each cluster')
-    parser.add_argument("reads", help="Path to FastQ or Fasta file")
-    parser.add_argument("out_directory", help="Path to work directory for output files where will be saved")
-    #parser.add_argument("-path_TH",  help="Path to the location of the TideHunter")
+    parser.add_argument("-r","--reads", help="Path to FastQ or Fasta file")
+    parser.add_argument("-o","--out_directory", help="Path to work directory for output files where will be saved",default='/run_NTRF')
+    parser.add_argument("-m","--max_abundancy", help="The proportion of amount lengths all tandem repeats in one cluster to length all the reads",default=0.0001)
+    parser.add_argument("-consensus_name",help="File name with consensus sequences, default name is 'consensus.fasta.'",
+                        default='consensus.fasta')
+    parser.add_argument("-th","--threads", help="Number of threads for running the module Blast", default=4)
+    parser.add_argument("-log_file",
+                        help="This file list analysis parameters, modules and files, contains messages generated on the various stages of the NanoTRF work. "
+                             "It allows tracking events that happens when NanoTRF runs. Default - loging.log",
+                        default='loging.log')
+    parser.add_argument("-min_Overlap", help="Number of overlapping nucleotides  between repeats in one cluster", default=15)
+    parser.add_argument("-path_TH", help="Path to the location of the TideHunter",
+                        default=r'/home/ikirov/Tools/TideHunter-v1.4.2/bin/TideHunter')
+    parser.add_argument("-opt_delete",help="Remove unncessary large files and directories from working directory", default="d")
     args = parser.parse_args()
     if not os.path.exists(args.reads):
         print("File {} not found!".format(args.reads))
     else:
         print("File {} found...".format(args.reads))
-        nanoTRF(args.reads, args.out_directory)
+        nanoTRF(args.reads, args.out_directory, minAbundancy=args.max_abundancy, consensus_name=args.consensus_name,
+                threads=args.threads, log_name=args.log_file, min_overlap=args.min_Overlap, pathTH=args.path_TH, opt_delete=args.opt_delete)
+
